ecc080cb6bce61852c42f6413d8ab148394261ed
root <root@LAPTOP-J7THBE6U>
2024-01-02 10:25:36 +0000

    I started the project by adding guessing functionaliy.

---
+++ hangman/milestone_2.py
@@ -0,0 +1,13 @@
+import random
+word_list = ["watermelon","pomegranate", "banana", "apple", "melon"]
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input("Please enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha(): 
+    print("Good guess!")
+else:   
+    print("Oops! That is not a valid input")
