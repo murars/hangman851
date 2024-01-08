d242ae855ea209542b7aba85cb93b38c02add319
Murat Arslan <marslan72@gmail.com>
2024-01-08 20:20:40 +0000

    Refines and updates the ode

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -2,64 +2,48 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        self.word = random.choice(word_list) 
-        self.word_guessed = ["_"] * len(self.word) # List to track the letters guessed so far
+        self.word = random.choice(word_list)
+        self.word_guessed = ["_"] * len(self.word)
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
-        self.word_list = word_list
-        self.list_of_guesses = [ ]
-        
-    def check_guess(self, guess):
+        self.list_of_letters = []
+        print(f"The mystery word has {len(self.word)} characters")
+        print(" ".join(self.word_guessed))
+
+    def check_letter(self, letter) -> None: # -> None is a part of the function's type hinting in Python, indicating that the function does not have a return value.
         """
         Checks if the guessed letter is in the chosen word.
         Prints a message indicating whether the guess is correct.
         """
-        
-        guess = guess.lower()
-        correct_guess = False
-        
-        if guess in self.word:  
-            for i, letter in enumerate(self.word):
-                if letter == guess and self.word_guessed[i] == '_' :
-                    self.word_guessed[i] = guess 
-                    correct_guess = True # set correct_guess condution to determine to win or lose
-        
-            if  correct_guess: 
-                print(f"Good guess! {guess} is in the word.")
-                self.num_letters -=1
-            
-        else :
-            self.num_lives -=1
-            print(f"Sorry, {guess} is not in the word.")
-            print(f"You have {self.num_lives} lives left.")  
-                  
-    def ask_for_input(self):
+        letter = letter.lower()
+        if letter in self.word:
+            if letter not in self.list_of_letters:
+                self.num_letters -= 1
+            for i, current_letter in enumerate(self.word):
+                if current_letter == letter:
+                    self.word_guessed[i] = letter
+            print(" ".join(self.word_guessed))
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {letter} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_letter(self):
         """
         Continuously prompts the user to enter a single letter.
         Calls check_guess function to verify the guess.
         Exits the loop if a valid letter is guessed.Continues prompting until the game is won or the player runs out of lives.
         """
         while True:
-            guess = input("Please enter a single letter: ")
-            if not guess.isalpha() and len(guess) !=1:
-                print("Invalid letter. Please, enter a single alphabetical character.")
-            
-            elif guess in self.list_of_guesses:
-                print( "You already tried that letter!")
+            letter = input("Please enter a single letter: ")
+            if len(letter) != 1 or not letter.isalpha():
+                print("Please, enter just one character")
+                
+            elif letter in self.list_of_letters:
+                print(f"{letter} was already tried.")
             
             else:
-                self.check_guess(guess)
-                self.list_of_guesses.append(guess)
-                
-                # Check for win or lose conditions                          
-                if self.num_lives <= 0:
-                    print("Game over! Please try again!")                    
-                    break                        
-                
-                elif "_" not in self.word_guessed: 
-                    print(f"Congratulations! You've guessed the word: {self.word}")
-                    break        
-                      
-word_list = ["apple", "banana", "cherry"]
-game = Hangman(word_list)
-game.ask_for_input()
+                self.list_of_letters.append(letter)
+                self.check_letter(letter)
+                break
+
