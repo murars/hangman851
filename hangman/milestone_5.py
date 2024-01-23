d3858eebffcb7ccd03f31a43a3e25ad3c5d2e471
MURAT ARSLAN <halil.serkanuz@journi.uk>
2024-01-23 22:25:52 +0000

    Refined the code and comments.

--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -1,7 +1,7 @@
 import random
 
 class Hangman:
-    def __init__(self, word_list, num_lives=5):
+    def __init__(self, word_list : list, num_lives=5): 
         self.word = random.choice(word_list)
         self.word_guessed = ["_"] * len(self.word)
         self.num_letters = len(set(self.word))
@@ -9,8 +9,8 @@ class Hangman:
         self.list_of_letters = []
         print(f"The word has {len(self.word)} characters")
         print(" ".join(self.word_guessed))
-
-    def check_letter(self, letter) -> None: # -> None is a part of the function's type hinting in Python, indicating that the function does not have a return value.
+    
+    def check_letter(self, letter : str) -> None: # -> None is a part of the function's type hinting in Python, indicating that the function does not have a return value.
         """
         Checks if the guessed letter is in the chosen word.
         Prints a message shows if the guess is correct.
@@ -19,13 +19,14 @@ class Hangman:
         if letter in self.word:
             if letter not in self.list_of_letters:
                 self.num_letters -= 1
-            for i, current_letter in enumerate(self.word):# index yaratmak icin kullandik.
+            for i, current_letter in enumerate(self.word):
                 if current_letter == letter:
                     self.word_guessed[i] = letter
             print(" ".join(self.word_guessed))
         else:
             self.num_lives -= 1
             print(f"Sorry, {letter} is not in the word.")
+            print(" ".join(self.word_guessed))
             print(f"You have {self.num_lives} lives left.")
 
     def ask_letter(self):
@@ -48,7 +49,7 @@ class Hangman:
                 self.check_letter(letter)
                 break
 
-def play_hangman(word_list):
+def play_hangman(word_list : list):
     game = Hangman(word_list)
     while True:
         game.ask_letter()
@@ -62,7 +63,6 @@ def play_hangman(word_list):
 if __name__ == '__main__':
     word_list = ["watermelon","pomegranate", "banana", "apple", "melon"]
     play_hangman(word_list)
-
 
 
 
